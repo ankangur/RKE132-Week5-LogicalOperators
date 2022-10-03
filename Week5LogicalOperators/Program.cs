0ff1151b5e31ad3822770d02a5573c33b73473d5
Annika Kangur <annika_kangur@hotmail.com>
2022-10-03 22:33:32 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,10 @@
+﻿//AND - &&
+//TIGIMUSED PEAVAD LÄBIMA KONTROLLI JA TAGAMA TRUE VÄÄRTUSE - kõik tingimused peavad olema täidetud 
+//true (keemia eksam) && true (matem eksam) && true (füüsika eksam)--> true
+// true (keemia eksam) && false (matem eksam) --> false
+//false && false --> false
+
+//OR - || (pipes) - VÄHEMALT ÜKS TINGIMUSTEST ON TÄIDETUD
+//true (keemia eksam) || true (matem eksam) || true (füüsika eksam)--> true
+//true (keemia eksam) || false (matem eksam)--> true
+//false || false --> false
